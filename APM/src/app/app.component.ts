81c26a590e82f2ae55fff762ea0fe27fbef7c6e8
Peter Partridge <petertoby@aol.com>
2018-07-01 12:40:37 +0100

    Adding components to the Angular apoplication

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-@Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({ // '' allows mutiline html in es 2015
+  selector: "pm-root", 
+  templateUrl:"app.component.html"
+
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
+export class AppComponent{
+  pageTitle: string = "Acme Product Mangment";
+  title: string = "World"
 }
