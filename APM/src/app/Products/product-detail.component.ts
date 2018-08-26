23f3d1d8999d0d18c004a20d70f6f4a50bd263f2
Peter Partridge <petertoby@aol.com>
2018-08-26 15:59:07 +0100

    routing and gaurds
    Tutoral was fussy on the gaurd section. You must import a gaurd. Tutorial made out boilerplate did it all for you it doesnt.

--- APM/src/app/Products/product-detail.component.ts
+++ APM/src/app/Products/product-detail.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './Product';
-
+import {ActivatedRoute, Router} from '@angular/router';
+import {IProduct} from './Product';
 @Component({
-  selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 
 export class ProductDetailComponent implements OnInit {
- pageTitle: string = 'Product Details';
- product: IProduct;
+pageTitle: string = 'Product Details';
+product: IProduct;
+ constructor(private route: ActivatedRoute, private router: Router) {}
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `${id}`;
+    this.product = {
+      'productId': id,
+        'productName': 'Garden Cart',
+        'productCode': 'GDN-0023',
+        'releaseDate': 'March 18, 2016',
+        'description': '15 gallon capacity rolling garden cart',
+        'price': 32.99,
+        'starRating': 4.2,
+        'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
+    };
+}
 
- constructor() { }
-
-  ngOnInit(private product: IProduct) {
-    this
-  }
-
+onBack(): void {
+  this.router.navigate(['/products']);
+}
 }
--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,24 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './Products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { starComponent } from './shared/starComponent';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './Products/product-detail.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    starComponent
+    starComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id',
+      canActivate: [ProductDetailGuard],
+      component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '***', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent, ProductListComponent]
